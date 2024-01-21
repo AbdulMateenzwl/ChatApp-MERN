5f56bd13bb37eac99e585ac4bf688aba0142be3c
AbdulMateenzwl <abdulmateenkhanzwl@gmail.com>
2024-01-21 21:50:58 +0500

    Add user registration functionality

--- backend/server.js
+++ backend/server.js
@@ -3,24 +3,17 @@ const dotenv = require("dotenv")
 const {chats}  = require("./data/data")
 const connectDB = require("./config/db")
 const colors = require("colors")
+const userRoutes = require("./routes/userRoutes")
 
 const app = express()
 
 dotenv.config()
 connectDB()
 
-app.get("/", (req, res) => {
-    res.send("API is running")
-})
+app.use(express.json()) // to accept json data in the body
 
-app.get("/api/chat",(req,res)=>{
-    res.send(chats)
-})
+app.use("/api/user",userRoutes)
 
-app.get("/api/chat/:id",(req,res)=>{
-    const singleChat = chats.find((chat)=>chat._id === req.params.id)
-    res.send(singleChat)
-})
 
 const PORT = process.env.PORT || 5000
 
