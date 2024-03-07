3e3dc02828a6b4249f6f042b6ae8533edea1af79
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2024-03-07 19:42:10 +0500

    Add messageRoutes, message controllers

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/db")
 const colors = require("colors")
 const userRoutes = require("./routes/userRoutes")
 const chatRoutes = require("./routes/chatRoutes")
+const messageRoutes = require("./routes/messageRoutes")
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware")
 
 const app = express()
@@ -14,7 +15,8 @@ connectDB()
 app.use(express.json()) // to accept json data in the body
 
 app.use("/api/user", userRoutes)
-app.use("/api/chat",chatRoutes)
+app.use("/api/chat", chatRoutes)
+app.use("/api/message", messageRoutes)
 
 
 app.use(notFound)
