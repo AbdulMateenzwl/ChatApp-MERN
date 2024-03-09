a783c27836ba62aba98cae143daaaec7642ab250
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2024-03-09 16:12:44 +0500

    Add react-scrollable-feed package and implement ScrollableChat component

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Box, Text } from '@chakra-ui/layout'
 import { ChatState } from '../Context/ChatProvider'
 import { IconButton } from '@chakra-ui/button'
@@ -6,9 +6,90 @@ import { ArrowBackIcon } from '@chakra-ui/icons'
 import { getSender, getSenderFull } from '../config/ChatLogics'
 import ProfileModal from './miscellaneous/ProfileModal'
 import UpdateGroupChatModal from './miscellaneous/UpdateGroupChatModal'
+import { Spinner, FormControl, useToast } from '@chakra-ui/react'
+import { Input } from '@chakra-ui/input'
+import { useState } from 'react'
+import axios from "axios";
+import ScrollableChat from "./ScrollableChat";
 
 export default function SingleChat({ fetchAgain, setFetchAgain }) {
+
+	const [messages, setMessages] = useState([])
+	const [loading, setLoading] = useState(false)
+	const [newmessage, setNewmessage] = useState()
+
+	const toast = useToast()
+
+
 	const { user, selectedChat, setSelectedChat } = ChatState()
+
+	const sendMessage = async (e) => {
+		if (e.key === "Enter" && newmessage !== "") {
+			try {
+				const config = {
+					headers: {
+						'Content-Type': 'application/json',
+						'Authorization': `Bearer ${user.token}`
+					}
+				}
+
+				const { data } = await axios.post('/api/message', {
+					content: newmessage,
+					chatId: selectedChat._id,
+				},
+					config
+				)
+				setNewmessage('')
+				setMessages([...messages, data])
+				console.log(data)
+			}
+			catch (e) {
+				toast({
+					title: "Failed to send message",
+					description: "Please try again",
+					status: "error",
+					duration: 3000,
+					isClosable: true,
+				})
+			}
+		}
+	}
+
+	const fetchMessages = async () => {
+		if (!selectedChat) return
+
+		try {
+			const config = {
+				headers: {
+					Authorization: `Bearer ${user.token}`
+				},
+			}
+			setLoading(true)
+
+			const { data } = await axios.get(`/api/message/${selectedChat._id}`, config)
+			console.log(data)
+			setMessages(data)
+			setLoading(false)
+		}
+		catch (e) {
+			toast({
+				title: "Failed to fetch messages",
+				description: "Please try again",
+				status: "error",
+				duration: 3000,
+				isClosable: true,
+			})
+		}
+	}
+
+	const typingHandler = (e) => {
+		setNewmessage(e.target.value)
+		// Typing Indicator Logic
+	}
+
+	useEffect(() => {
+		fetchMessages();
+	}, [selectedChat])
 
 	return (
 		<>
@@ -38,7 +119,9 @@ export default function SingleChat({ fetchAgain, setFetchAgain }) {
 									{selectedChat.chatName.toUpperCase()}
 									<UpdateGroupChatModal
 										fetchAgain={fetchAgain}
-										setFetchAgain={setFetchAgain} />
+										setFetchAgain={setFetchAgain}
+										fetchMessages={fetchMessages}
+									/>
 								</>
 							)}
 
@@ -54,7 +137,39 @@ export default function SingleChat({ fetchAgain, setFetchAgain }) {
 							borderRadius={"lg"}
 							overflowY={'hidden'}
 						>
-							{/* Messages Here */}
+							{loading ? (<Spinner w={20} h={20} alignSelf={'center'} margin={'auto'} />) :
+								(
+									<div className="messages">
+										<ScrollableChat messages={messages} />
+									</div>
+								)
+							}
+							<FormControl
+								onKeyDown={sendMessage}
+								id="first-name"
+								isRequired
+								mt={3}
+							>
+								{/* {istyping ? (
+									<div>
+										<Lottie
+											options={defaultOptions}
+											// height={50}
+											width={70}
+											style={{ marginBottom: 15, marginLeft: 0 }}
+										/>
+									</div>
+								) : (
+									<></>
+								)} */}
+								<Input
+									variant="filled"
+									bg="#E0E0E0"
+									placeholder="Enter a message.."
+									value={newmessage}
+									onChange={typingHandler}
+								/>
+							</FormControl>
 						</Box>
 					</>
 				) : (
